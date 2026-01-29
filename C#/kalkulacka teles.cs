d5da5c4eaa8b76c18d984b734be1d29d4c5a0361
Viktor Knespl <140974961+knespii@users.noreply.github.com>
2026-01-29 22:47:02 +0100

    Add files via upload

---
+++ C#/kalkulacka teles.cs
@@ -0,0 +1,42 @@
+﻿bool teleso;
+
+Console.WriteLine("vyber teleso(1=obvod kruhu 0= objem valce)" );
+string s = Console.ReadLine();
+if (s == "1")
+{
+    teleso = true;
+}
+else if (s == "0")
+{
+    teleso = false;
+}
+else
+{
+    Console.WriteLine("Neplatná volba!");
+    return;
+}
+if (teleso == true) {
+    double polomer;
+    Console.WriteLine("zadej polomer");
+    string polomerString = Console.ReadLine();
+    polomer = Convert.ToDouble( polomerString );
+    Console.WriteLine($"polomer je: {Math.PI * 2 * polomer} ");
+    return;
+}
+
+if (teleso == false)
+{
+    double polomer;
+    Console.WriteLine("zadej polomer");
+    string polomerString = Console.ReadLine();
+    polomer = Convert.ToDouble(polomerString);
+
+    double vyska;
+    Console.WriteLine("zadej vysku:");
+   string vyskaString = Console.ReadLine();
+    vyska = Convert.ToDouble( vyskaString );
+
+    Console.WriteLine($"obsah valce je: {Math.PI * Math.Sqrt(polomer) * vyska}");
+    return;
+
+}
